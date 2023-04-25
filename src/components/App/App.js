d0ebaa729c9d487d209fff160ce5cb8e0b959fca
Lobov Evgeniy <lobov.em@gmail.com>
2023-04-25 18:41:12 +0200

    Edit code burger menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,18 @@ import { burgerMenuSelector } from '../store/reducer';
 import s from './app.module.scss';
 import { Header } from '../Header/Header';
 import { Banner } from '../Banner/Banner';
+import { useEffect } from 'react';
 
 function App() {
   const burgerMenu = useSelector(burgerMenuSelector);
+
+  useEffect(() => {
+    if (burgerMenu) {
+      document.body.classList.add('active');
+    } else {
+      document.body.classList.remove('active');
+    }
+  }, [burgerMenu]);
 
   return (
     <div className={!burgerMenu ? `${s.body}` : `${s.body} ${s.active}`}>
--- src/components/kit/Burger/Burger.jsx
+++ src/components/kit/Burger/Burger.jsx
@@ -6,6 +6,11 @@ export const Burger = () => {
   const burgerMenu = useSelector(burgerMenuSelector);
   const dispatch = useDispatch();
 
+  // const handleClick = () => {
+  //   // 👇️ toggle class on the body element
+  //   document.body.classList.add('active');
+  // };
+
   return (
     <div className={!burgerMenu ? s.burger : `${s.burger} ${s.active}`} onClick={() => dispatch(isOpenBurgerMenu())}>
       <span className={s.burger__line}></span>
