eb36c8c86621a3af8f95ab7d1c56512741bc2769
Lobov Evgeniy <lobov.em@gmail.com>
2023-04-27 16:10:21 +0200

    Maked decor section

--- src/components/SectionNews/SectionsNews.jsx
+++ src/components/SectionNews/SectionsNews.jsx
@@ -6,16 +6,21 @@ export const SectionNews = (props) => {
   return (
     <div className={s.sectionNews}>
       <div className={s.sectionNews__wrap}>
+        <h2 className={s.sectionNews__decor}>Новини</h2>
         <TitleSection title="Новини"></TitleSection>
-        <div className={s.sectionNews__textWrap}>
-          <TitleMain title="Нашою метою є вирішення нагальних проблем ЗСУ"></TitleMain>
-          <p className={s.sectionNews__title}>
-            За допомогою онлайн -генератора тексту ви можете обробляти свій особистий Lorem Ipsum , збагачуючи його html -елементами, які визначають його
-            структуру, з можливістю вставляти зовнішні посилання, але не тільки...
-          </p>
-          <a className={s.sectionNews__link} href="/">
-            Читати повністю
-          </a>
+        <div className={s.sectionNews__inner}>
+          <div className={s.sectionNews__textWrap}>
+            <TitleMain title="Нашою метою є вирішення нагальних проблем ЗСУ"></TitleMain>
+            <p className={s.sectionNews__title}>
+              За допомогою онлайн -генератора тексту ви можете обробляти свій особистий Lorem Ipsum , збагачуючи його html -елементами, які визначають його
+              структуру, з можливістю вставляти зовнішні посилання, але не тільки...
+            </p>
+            <a className={s.sectionNews__link} href="/">
+              Читати повністю
+            </a>
+
+            <div className={s.sectionNews__slider}>SLIDER</div>
+          </div>
         </div>
       </div>
     </div>
