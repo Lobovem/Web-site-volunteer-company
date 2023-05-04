70de6424805d64a58f025bc3f347ea8bf39e3b3a
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-04 17:31:00 +0200

    I do decor section of support

--- src/components/WaySupport/WaySupport.jsx
+++ src/components/WaySupport/WaySupport.jsx
@@ -6,6 +6,7 @@ export const WaySupport = () => {
   return (
     <div className={s.waySupport}>
       <div className={s.waySupport__wrap}>
+        <h2 className={s.waySupport__decor}>Підтримка</h2>
         <TitleSection title="напрямки підтримки"></TitleSection>
         <SliderThird></SliderThird>
       </div>
