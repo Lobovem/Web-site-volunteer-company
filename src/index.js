030e19e61fe39ac42e436177432accde0c031732
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-16 23:13:20 +0200

    Added scroll top

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './components/App/reportWebVitals';
 import { store } from './redux/config/store';
+import ScrollToTop from './components/ScrollTop/ScrollTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
