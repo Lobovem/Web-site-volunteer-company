7ca2db515b9b2b472c050b7922a7aae9317b8db9
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-19 12:17:44 +0200

    I changed className to footer social link

--- src/components/FooterContacts/FooterContacts.jsx
+++ src/components/FooterContacts/FooterContacts.jsx
@@ -10,17 +10,19 @@ export const FooterContacts = () => {
         <h2 className={s.contacts__title}>контакти</h2>
         <div className={s.contacts__phoneBox}>
           <img className={s.contacts__img} src={phone} alt="icon-phone.svg" />
-          <a className={s.contacts__desc} href="tel:+380630494002">
+          <a className={s.contacts__link} href="tel:+380630494002">
             +380 (63)-049-40-02
           </a>
-          <a className={s.contacts__desc} href="tel:+380671597373">
+          <a className={s.contacts__link} href="tel:+380671597373">
             +380 (67)-159-73-73
           </a>
         </div>
 
         <div className={s.contacts__adressBox}>
           <img className={s.contacts__img} src={adress} alt="icon-adress.svg" />
-          <address className={s.contacts__desc}>м. Харків, вул.Космічна, буд. 21, оф. 529</address>
+          <a className={s.contacts__link} href="https://goo.gl/maps/uXTuc6J6nSarrwsW7" target="blank">
+            <address className={s.contacts__desc}>м. Харків, вул.Космічна, буд. 21, оф. 529</address>
+          </a>
         </div>
       </div>
     </div>
