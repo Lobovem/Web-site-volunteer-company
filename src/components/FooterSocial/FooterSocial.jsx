c8461b56a518796efd8a89ba9a86cdc3243af3ff
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-24 19:06:42 +0200

    Revert "Done path img"
    This reverts commit 5463790f7041f7ce2538a31bc71a8920df0b8845.

--- src/components/FooterSocial/FooterSocial.jsx
+++ src/components/FooterSocial/FooterSocial.jsx
@@ -1,15 +1,12 @@
 import s from './FooterSocial.module.scss';
 
-import facebook from './icon-facebook.svg';
-import instagram from './icon-instagram.svg';
-
 export const FooterSocial = () => {
   return (
     <div className={s.social}>
       <div className={s.social__wrap}>
         <h2 className={s.social__title}>соціальні мережі</h2>
         <a className={s.social__wrapLink} target="blank" href="https://www.instagram.com/ukrhumbat/?igshid=YmMyMTA2M2Y=">
-          <img className={s.social__img} src={instagram} alt="icon-instagram.svg" />
+          <img className={s.social__img} src="./img/icon-instagram.svg" alt="icon-instagram.svg" />
           <p className={s.social__desc}>instagram</p>
         </a>
 
@@ -18,7 +15,7 @@ export const FooterSocial = () => {
           target="blank"
           href="https://www.facebook.com/people/%D0%9C%D0%91%D0%A4-%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B9-%D0%B3%D1%83%D0%BC%D0%B0%D0%BD%D1%96%D1%82%D0%B0%D1%80%D0%BD%D0%B8%D0%B9-%D0%B1%D0%B0%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD/100085936770532/"
         >
-          <img className={s.social__img} src={facebook} alt="icon-facebook.svg" />
+          <img className={s.social__img} src="./img/icon-facebook.svg" alt="icon-facebook.svg" />
           <p className={s.social__desc}>facebook</p>
         </a>
       </div>
--- src/components/LogoMain/LogoMain.jsx
+++ src/components/LogoMain/LogoMain.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import s from './LogoMain.module.scss';
-import foto from './logo-main.svg';
 
 export const LogoMain = () => {
   return (
     <Link to="/" className={s.logoMain}>
-      <img src={foto} alt="ukraine-flag.png" />
+      <img src="./img/logo-main.svg" alt="ukraine-flag.png" />
     </Link>
   );
 };
