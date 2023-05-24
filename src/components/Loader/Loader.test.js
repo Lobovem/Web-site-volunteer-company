300e64b9dd9e2ba7e38dd694f62a759eba3823a0
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-24 11:33:53 +0200

    Done Loader test

---
+++ src/components/Loader/Loader.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Loader } from './Loader';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/config/store';
+
+describe('Loader` component', () => {
+  test('renders Loader component', () => {
+    // eslint-disable-next-line testing-library/render-result-naming-convention
+    const component = render(
+      <Provider store={store}>
+        <Router>
+          <Loader />
+        </Router>
+      </Provider>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
